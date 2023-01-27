e4b3ea53266259c5f59e8f41b185a9f35a23e391
Akshith-Patil <apat0017@student.monash.edu>
2023-01-27 16:07:04 +1100

    Updated env

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 import { logo } from "../utils/constants";
@@ -8,6 +8,19 @@ const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
+      <Typography
+        variant="h4"
+        sx={{
+          color: "white",
+          paddingLeft: 1,
+          fontFamily: "Roboto, sans-serif",
+          fontWeight: 700,
+          fontSize: 30,
+          marginBottom: 0.5,
+        }}
+      >
+        MeTube
+      </Typography>
     </Link>
     <SearchBar />
   </Stack>
